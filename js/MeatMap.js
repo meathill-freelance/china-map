7bc1484b4588a6eb9edef3730ba35e9153d9590b
Meathill <meathill@gmail.com>
2015-07-13 09:31:01 +0800

    重新格式化配置，以便增加其它操作
    Signed-off-by: Meathill <meathill@gmail.com>

--- js/MeatMap.js
+++ js/MeatMap.js
@@ -2,45 +2,6 @@
 (function ($) {
   var root = this;
 
-  var config = {
-    'asset': '../img/china.svg',
-    'provinces': [
-      "海南",
-      "广东",
-      "云南",
-      "广西",
-      "台湾",
-      "福建",
-      "贵州",
-      "江西",
-      "湖南",
-      "四川",
-      "西藏",
-      "浙江",
-      "重庆",
-      "湖北",
-      "安徽",
-      "江苏",
-      "河南",
-      "陕西",
-      "青海",
-      "山西",
-      "山东",
-      "宁夏",
-      "河北",
-      "新疆",
-      "内蒙古",
-      "北京&天津",
-      "辽宁",
-      "吉林",
-      "黑龙江",
-      "甘肃",
-      "上海",
-      "香港",
-      "澳门"
-    ]
-  };
-
   var Map = function (options) {
     this.width = options.width;
     this.height = options.height;
@@ -67,18 +28,20 @@
           return true;
         }
         var box = area.getBBox()
-          , label = self.el.text(0, 0, config.provinces[i - 3])
+          , obj = Map.config.provinces[i - 3]
+          , label = self.el.text(0, 0, obj.label)
           , lbox = label.getBBox();
         label.attr({
           x: box.x + (box.width >> 1),
           y: box.y + (box.height >> 1)
         });
         self.labels.push(label);
+        obj.eid = area.id;
       });
       this.labels.toFront();
     },
     loadMapSource: function () {
-      $.get(config.asset, $.proxy(this.mapSource_fetchedHandler, this), 'html');
+      $.get(Map.config.asset, $.proxy(this.mapSource_fetchedHandler, this), 'html');
     },
     mapSource_fetchedHandler: function (svg) {
       var doc = $.parseXML(svg);
---
+++ js/config.js
@@ -0,0 +1,140 @@
+'use strict';
+(function ($, ns) {
+  ns.config = {
+    'asset': '../img/china.svg',
+    'provinces': [
+      {
+        id: 'hainan',
+        label: "海南"
+      },
+      {
+        id: 'guangdong',
+        label: "广东"
+      },
+      {
+        id: 'yunnan',
+        label: "云南"
+      },
+      {
+        id: 'guangxi',
+        label: "广西"
+      },
+      {
+        id: 'taiwan',
+        label: "台湾"
+      },
+      {
+        id: 'fujian',
+        label: "福建"
+      },
+      {
+        id: 'guizhou',
+        label: "贵州"
+      },
+      {
+        id: 'jiangxi',
+        label: "江西"
+      },
+      {
+        id: 'hunan',
+        label: "湖南"
+      },
+      {
+        id: 'sichuan',
+        label: "四川"
+      },
+      {
+        id: 'xizang',
+        label: "西藏"
+      },
+      {
+        id: 'zhejiang',
+        label: "浙江"
+      },
+      {
+        id: 'chongqing',
+        label: "重庆"
+      },
+      {
+        id: 'hubei',
+        label: "湖北"
+      },
+      {
+        id: 'anhui',
+        label: "安徽"
+      },
+      {
+        id: 'jiangsu',
+        label: "江苏"
+      },
+      {
+        id: 'henan',
+        label: "河南"
+      },
+      {
+        id: 'shanxi',
+        label: "陕西"
+      },
+      {
+        id: 'qinghai',
+        label: "青海"
+      },
+      {
+        id: 'shanxi2',
+        label: "山西"
+      },
+      {
+        id: 'shandong',
+        label: "山东"
+      },
+      {
+        id: 'ningxia',
+        label: "宁夏"
+      },
+      {
+        id: 'hebei',
+        label: "河北"
+      },
+      {
+        id: 'xinjiang',
+        label: "新疆"
+      },
+      {
+        id: 'neimenggu',
+        label: "内蒙古"
+      },
+      {
+        id: 'beijing',
+        label: "北京&天津"
+      },
+      {
+        id: 'liaoning',
+        label: "辽宁"
+      },
+      {
+        id: 'jilin',
+        label: "吉林"
+      },
+      {
+        id: 'heilongjiang',
+        label: "黑龙江"
+      },
+      {
+        id: 'gansu',
+        label: "甘肃"
+      },
+      {
+        id: 'shanghai',
+        label: "上海"
+      },
+      {
+        id: 'hongkong',
+        label: "香港"
+      },
+      {
+        id: 'macaw',
+        label: "澳门"
+      }
+    ]
+  };
+}(jQuery, MeatMap));
